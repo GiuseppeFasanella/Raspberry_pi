c65b385d4a93867f1c52d5567e486354542f2317
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-07-13 22:37:55 +0200

    Update alarm_clock.sh

--- alarm_clock.sh
+++ alarm_clock.sh
@@ -1,10 +1,5 @@
 #! /bin/bash                                                                                                                                                                     
 
-#To-do: 
-#change $1 into the absolute path         
-#allow the script to play several times the same file
-#how to kill everyything?
-
 cd $1
 
 play(){
@@ -14,8 +9,8 @@ play(){
         file_to_play=$(head -1 list.dat)
         #echo "file to play is " ${file_to_play}                                                                                                                                 
         #Use the absolute path, otherwise it doesn't work properly                                                                                                               
-        /usr/bin/mplayer /home/gfasanel/$1${file_to_play}
-        sed -i "/${file_to_play}/d" /home/gfasanel/$1list.dat
+        /usr/bin/mplayer $1${file_to_play}
+        sed -i "/${file_to_play}/d" $1list.dat
     done
 }
 
@@ -28,3 +23,5 @@ else
 fi
 
 cd -
+
+
