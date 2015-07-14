e201bc8aa5bb5e80a0ef33c3743ed1087638da0b
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-07-14 14:21:50 +0200

    Create killer.sh

---
+++ killer.sh
@@ -0,0 +1,2 @@
+#! /bin/bash                                                                                                                                                                                                        
+ps -o pid,sess,cmd afx | grep -A20 "cron$" | grep "/home/gfasanel/bin/alarm_clock.sh" | head -1 | cut -f2 -d ' '
