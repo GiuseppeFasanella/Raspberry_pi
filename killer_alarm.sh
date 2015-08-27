c3fa321637b549bc09b15a3de41be039397d9c0a
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-08-27 10:27:19 +0200

    Delete killer_alarm.sh

--- killer_alarm.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash                                                                                                                                                                                                        
-pkill -s $(ps -o pid,sess,cmd afx | grep -A20 "cron$" | grep "/home/gfasanel/bin/alarm_clock.sh" | head -1 | cut -f2 -d ' ')
