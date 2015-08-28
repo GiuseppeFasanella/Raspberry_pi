919d933a0a2a75a7b7b4d2f53b4aab2f0bf736ba
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-08-28 21:01:39 +0200

    Update alarm_clock_pi.sh

--- Alarm_clock/alarm_clock_pi.sh
+++ Alarm_clock/alarm_clock_pi.sh
@@ -1,26 +1,32 @@
-#! /bin/bash                                                                                                                                                                     
-
+#! /bin/bash
 cd $1
 
 play(){
     while :
     do
-        #echo "playing file from list.dat"                                                                                                                                       
+        #echo "playing file from list.dat"
         file_to_play=$(head -1 list.dat)
-        #echo "file to play is " ${file_to_play}                                                                                                                                 
-        #Use the absolute path, otherwise it doesn't work properly                                                                                                               
-        usr/bin/omxplayer $1${file_to_play}
-        sed -i "/${file_to_play}/d" $1list.dat
+        #echo "file to play is " ${file_to_play}
+        #Use the absolute path, otherwise it doesn't work properly
+        /usr/bin/omxplayer $1${file_to_play}
+        sed -i "/${file_to_play}/d" list.dat
+
+        #if size of list.dat is 0 (you have just removed the last .mp3 in the
+list): re-generate list.dat
+        if [ ! -s list.dat ]; then
+            ls -v > list.dat; sed -i "/list.dat/d" list.dat
+        fi
+
     done
 }
 
-# play file if list.dat exists and it's not empty (size is not zero)
+#play file if list.dat exists and it's not empty (size is not zero)
 if [ -e list.dat ] && [ -s list.dat ];then
     play $1
 else
-    #ls -v: ordina in base ai numeri cardinali 1,2,3,..... altimenti farebbe 10,1,2,3,4,5....
+    #ls -v: numerical order
     ls -v > list.dat; sed -i "/list.dat/d" list.dat;
-    #list.dat should not be in the list of files                                                                                                                                 
+    #list.dat should not be in the list of files
     play $1
 fi
 
