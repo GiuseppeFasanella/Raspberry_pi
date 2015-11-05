27f4d6fd24adca8cf4b07d504ff2a8e8ff9214a5
giuseppe from raspberry <pi@raspberrypi.(none)>
2015-11-05 19:21:33 +0000

    fixing sed removing playlist

--- Alarm_clock/alarm_clock_pi.sh
+++ Alarm_clock/alarm_clock_pi.sh
@@ -4,18 +4,17 @@ cd $1
 play(){
     while :
     do
-        #echo "playing file from list.dat"                                                                                                                                       
+        #echo "playing file from list.dat"                                                                                                                                     
         file_to_play=$(head -1 list.dat)
-        #echo "file to play is " ${file_to_play}                                                                                                                                 
-        #Use the absolute path, otherwise it doesn't work properly                                                                                                               
-        /usr/bin/omxplayer $1${file_to_play}
-        sed -i "/${file_to_play}/d" list.dat
+        #echo "file to play is " ${file_to_play}                                                                                                                                       #Use the absolute path, otherwise it doesn't work properly
+	/usr/bin/omxplayer $1${file_to_play}
+        sed -i "/^${file_to_play}/d" list.dat
 
 	#if size of list.dat is 0 (you have just removed the last .mp3 in the list): re-generate list.dat
 	if [ ! -s list.dat ]; then
-	    ls -v > list.dat; sed -i "/list.dat/d" list.dat
+	    #ls -v > list.dat; sed -i "/list.dat/d" list.dat
+	    cp full_list.dat list.dat; 
 	fi
-
     done
 }
 
@@ -24,8 +23,9 @@ if [ -e list.dat ] && [ -s list.dat ];then
     play $1
 else
     #ls -v: numerical order
-    ls -v > list.dat; sed -i "/list.dat/d" list.dat;
-    #list.dat should not be in the list of files                                                                                                                                 
+    #ls -v > list.dat; sed -i "/list.dat/d" list.dat;
+    cp full_list.dat list.dat; 
+    #list.dat should not be in the list of files                                                                                                                               
     play $1
 fi
 
