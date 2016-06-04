30390ce0ee764a05910d97b059401e60bc71d987
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-06-04 14:58:00 +0200

    Create relay_on.py

---
+++ Domotica/Relay/relay_on.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python                                                                                                                                                              
+import RPi.GPIO    as GPIO
+#GPIO stands for general purpose input/output ports                                                                                                                             
+import time
+#this choices the name convention for the pins                                                                                                                                  
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23,GPIO.OUT)
+
+GPIO.output(23,GPIO.LOW)
+time.sleep(1)
