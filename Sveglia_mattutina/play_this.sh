1377062cbb581ee5c842992bc2e8249d400cf655
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-10-03 20:38:27 +0200

    Create play_this.sh

---
+++ Sveglia_mattutina/play_this.sh
@@ -0,0 +1,2 @@
+#! /bin/bash                                                                    
+/home/pi/Desktop/Sveglia_mattutina/alarm_clock_pi.sh /var/www/html/usb_drive_soft_link/Film,Musica,Libri,Partiture/Libri/Audiolibri/Pasolini/Ragazzi_di_vita/
